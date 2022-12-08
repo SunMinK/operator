22ffa90236a1c11c1a2786537ed8215ca6ff5273
SunMinK <sunnysunm98@gmail.com>
2022-12-08 14:10:30 +0900

    fivecommit

---
+++ src/edu/kh/op/practice/PracticeRun.java
@@ -0,0 +1,15 @@
+package edu.kh.op.practice;
+
+
+
+public class PracticeRun {
+ 
+	 public static void main(String [] args) {
+		 OpearPractice op = new OpearPractice();
+		 
+		 
+		 //op.practice1();
+		 //op.practice2();
+		 op.practice3();
+	 }
+}
